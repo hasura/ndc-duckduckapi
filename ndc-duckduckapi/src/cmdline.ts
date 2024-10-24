c1f40eeea299dcf64a15c390dfc78bf78e0e25de
Tanmai Gopal <tanmaig@hasura.io>
2024-10-23 21:37:04 -0700

    Starts process to add index to SDK

---
+++ ndc-duckduckapi/src/cmdline.ts
@@ -0,0 +1,38 @@
+import { Command, Option } from "commander"
+import { version } from "../package.json"
+import * as sdk from "@hasura/ndc-sdk-typescript";
+
+export type HostOptions = {
+  functions: string
+  schema: string
+  watch: boolean
+}
+
+export interface CommandActions {
+  serveAction(hostOpts: HostOptions, serverOpts: sdk.ServerOptions): Promise<void> | void
+}
+
+export function makeCommand(commandActions: CommandActions): Command {
+  const program = new Command()
+    .name("ndc-duckduckapi")
+    .version(version);
+
+  const serveCommand = sdk.getServeCommand();
+  serveCommand.action((serverOptions: sdk.ServerOptions, command: Command) => {
+    const hostOpts: HostOptions = hostCommand.opts();
+    return commandActions.serveAction(hostOpts, serverOptions);
+  })
+
+  const hostCommand = program
+    .command("host")
+    .addOption(
+      new Option("--watch", "watch for configuration changes and reload")
+        .default(false)
+        .env("WATCH")
+    )
+    .requiredOption("-f, --functions <filepath>", "path to your TypeScript functions file")
+    .requiredOption("-s, --schema <filepath>", "path to your SQL schema file")
+    .addCommand(serveCommand);
+
+  return program;
+}
---
+++ ndc-duckduckapi/src/host.ts
@@ -0,0 +1,44 @@
+// We must initialize OpenTelemetry instrumentation before importing any other module
+import * as sdkInstrumentation from "@hasura/ndc-sdk-typescript/instrumentation";
+sdkInstrumentation.initTelemetry("ndc-duckduckapi");
+import * as path from "path";
+import { readFileSync } from "fs";
+
+import * as sdk from "@hasura/ndc-sdk-typescript";
+import { makeConnector, duckduckapi } from "./sdk";
+import { makeCommand } from "./cmdline";
+
+
+const program = makeCommand({
+  serveAction: async (hostOpts, serveOpts) => {
+    const connectorConfig: duckduckapi = {
+      dbSchema: readFileSync(path.join(__dirname, hostOpts.schema), "utf-8"),
+      functionsFilePath: path.resolve(__dirname, hostOpts.functions),
+    };
+    const connector = await makeConnector(connectorConfig);
+
+    sdk.startServer(connector, serveOpts);
+  },
+});
+
+program.parseAsync().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
+
+
+// From original index.ts
+// import { start } from "@hasura/ndc-duckduckapi";
+// import { makeConnector, duckduckapi } from "@hasura/ndc-duckduckapi";
+// import * as path from "path";
+// import { readFileSync } from "fs";
+
+// const calendar: duckduckapi = {
+//   dbSchema: readFileSync(path.join(__dirname, "schema.sql"), "utf-8"),
+//   functionsFilePath: path.resolve(__dirname, "./functions.ts"),
+// };
+
+// (async () => {
+//   const connector = await makeConnector(calendar);
+//   start(connector);
+// })();
