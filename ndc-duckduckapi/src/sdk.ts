4f51a13fcbfd61f67231d0764117cb7c050c4109
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2025-01-16 20:30:34 +0530

    Multitenant data connector sdk

--- ndc-duckduckapi/src/sdk.ts
+++ ndc-duckduckapi/src/sdk.ts
@@ -1,8 +1,14 @@
 export { start } from "@hasura/ndc-sdk-typescript";
+export { Connection, Database } from "duckdb-async";
 export {
   makeConnector,
   duckduckapi,
   getDB,
   transaction,
   getOAuthCredentialsFromHeader,
+  getTenantDB,
+  tenants,
+  getTenantById,
+  Tenant,
+  TenantToken,
 } from "./duckduckapi";
